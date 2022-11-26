5f477470abfe50b8785070bb9bd7584ca9bcc9eb
nikitaaaa07 <92522600+nikitaaaa07@users.noreply.github.com>
2022-11-26 10:56:51 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,62 @@
+// Import all dependencies
+const dotenv = require('dotenv');
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+const app=express();
+
+//configure env file
+dotenv.config({path : './config.env'});
+require('./db/conn');
+const port = process.env.PORT;
+
+// Require Model
+const Users = require('./models/userSchema');
+const Message = require('./models/msgSchema');
+const authenticate = require('./middleware/authenticate')
+
+// These Method is Used to Get Data and Cookies from FrontEnd
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser());
+
+app.get('/', (req, res)=>{
+    res.send("Hello World");
+})
+
+// Registration
+app.post('/register', async (req, res)=>{
+    try {
+        // Get body or Data
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const createUser = new Users({
+            username : username,
+            email : email,
+            password : password
+        });
+
+        // Save Method is Used to Create User or Insert User
+        // But Before Saving or Inserting, Password will Hash 
+        // Because of Hashing. After Hash, It will save to DB
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("Registered");
+
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+
+
+
+
+//Run server
+app.listen(3001,()=>{
+    console.log("Server is Listening")
+})
